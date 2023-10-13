8981811d54a0c17199e91814a15de8a552695030
Shera Alice <sheraalice@students.amikom.ac.id>
2023-10-13 17:19:21 +0700

    menambahkan error handling

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const port = 3000
 
 app.use(express.json())
 app.use('/api/v1', require('./routes'))
+app.use((err, req, res, next) => {
+    return res.status(400).json({
+        status: false,
+        message: err.message,
+        data:null
+    });
+});
+
+app.use((req, res) => {
+    return res.status(400).json({
+        status: false,
+        message: 'Page not found',
+        data: null
+    });
+});
 
 app.listen(port,() => {
     console.log('Server is running')
